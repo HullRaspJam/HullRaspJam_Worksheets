08d49de2a178ce6db236657e374307a357ab6acf
Jon Witts <jwitts@qms.n-yorks.sch.uk>
2018-01-26 13:33:06 +0000

    Final worksheets for Python and Scratch Sorting Hats

--- Python_Sorting_Hat/Python Files/sorting-hat.py
+++ Python_Sorting_Hat/Python Files/sorting-hat.py
@@ -1,25 +1,25 @@
-#!/usr/bin/python3
-import time
-import random
-import os
+from time import sleep
+from random import randint
+from os import system
 from gpiozero import Button
 
 btn = Button(17)
 
 def randomHouse():
-    number = random.randint(1, 4)
+    number = randint(1,4)
+
     if number == 1:
-        os.system('mpg123 gryffindor.mp3')
-        time.sleep(1)
+        system('mpg123 gryffindor.mp3')
+        sleep(1)
     elif number == 2:
-        os.system('mpg123 hufflepuff.mp3')
-        time.sleep(1)
+        system('mpg123 hufflepuff.mp3')
+        sleep(1)
     elif number == 3:
-        os.system('mpg123 ravenclaw.mp3')
-        time.sleep(1)
+        system('mpg123 ravenclaw.mp3')
+        sleep(1)
     elif number == 4:
-        os.system('mpg123 slytherin.mp3')
-        time.sleep(1)
+        system('mpg123 slytherin.mp3')
+        sleep(1)
 
 while True:
     if btn.is_pressed:
--- Python_Sorting_Hat/Software/software.sh
+++ Python_Sorting_Hat/Software/software.sh
@@ -1,3 +1,1 @@
-#!/usr/bin/env bash
-sudo apt-get update
-sudo apt-get install mpg123 -y
+sudo apt-get update && sudo apt-get install mpg123 -y
